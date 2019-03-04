edca40d7a4e655b481d5f38623cc93b1f8b03409
Santiago <sgobotta@gmail.com>
2019-03-04 12:43:59 -0300

    Store: implements a lib module

---
+++ store/lib/helpers.js
@@ -0,0 +1,15 @@
+/**
+ * Generate reducer.
+ *
+ * @param {Object} initialState
+ * @param {Object} handlers
+ * @returns {function}
+ */
+export function createReducer(initialState: Object, handlers: Object): Function {
+  return function reducer(state: Object = initialState, action: Object): Object {
+    if ({}.hasOwnProperty.call(handlers, action.type)) {
+      return handlers[action.type](state, action);
+    }
+    return state;
+  };
+}
